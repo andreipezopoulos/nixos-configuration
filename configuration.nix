d1508207ee253112e2c3c6ced4d60053848d6956
Andrei Pezopoulos <not.my.email.yet@gmail.com>
2022-07-03 14:39:53 -0300

    Another

--- configuration.nix
+++ configuration.nix
@@ -3,8 +3,9 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
+
 let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz";
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz";
 in
 {
   imports =
@@ -17,9 +18,13 @@ in
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.grub.enable = true;
+  #boot.loader.grub.devices = [ "nodev" ];
+  #boot.loader.grub.efiSupport = true;
+  #boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "andrei-nixos";
-  # networking.wireless.enable = true;
+  networking.hostName = "andrei-nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
@@ -28,7 +33,9 @@ in
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking.useDHCP = false;
-  networking.interfaces.eno1.useDHCP = true;
+  networking.interfaces.enp45s0.useDHCP = true;
+  networking.interfaces.wlp0s20f3.useDHCP = true;
+  networking.resolvconf.dnsExtensionMechanism = false;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -47,12 +54,10 @@ in
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  
+
   # Configure keymap in X11
-  services.xserver.layout = "br";
-  services.xserver.xkbVariant = "abnt2";
-  # Use same config (above) for linux console
-  console.useXkbConfig = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
@@ -62,13 +67,37 @@ in
   hardware.pulseaudio.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
+
+  # Nvidia
+  hardware.nvidia.modesetting.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.nvidiaPersistenced = true;
+
+  # hardware.nvidia.powerManagement.enable = false;
+
+  hardware.nvidia.prime = {
+    sync.enable = true;
+    offload.enable = false;
+
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+    nvidiaBusId = "PCI:1:0:0";
+
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+    intelBusId = "PCI:0:2:0";
+  };
+
+  # https://discourse.nixos.org/t/internal-screen-not-detected-by-gnome-shell-when-logging-in-via-gdm-but-works-fine-with-lightdm/3853/12
+  services.xserver.displayManager.sessionCommands = ''
+    ${pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource modesetting NVIDIA-0
+    ${pkgs.xorg.xrandr}/bin/xrandr --auto
+  '';
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.andrei = {
     isNormalUser = true;
     home = "/home/andrei";
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "networkmanager" "docker" ]; # Enable ‘sudo’ for the user.
   };
 
   # List packages installed in system profile. To search, run:
@@ -76,13 +105,20 @@ in
   environment.systemPackages = with pkgs; [
     wget
     firefox
-    steam
     discord
     spotify
     kdiff3
+    pciutils
+    docker
+    docker-compose
+    notepadqq
+    emacs
   ];
   nixpkgs.config.allowUnfree = true;
-  programs.steam.enable = true;
+  virtualisation.docker.enable = true;
+
+  # emacs
+  # services.emacs.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -109,7 +145,7 @@ in
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
+  system.stateVersion = "21.11"; # Did you read the comment?
 
   # To be able to set GNOME themes via home-manager
   programs.dconf.enable = true;
@@ -123,7 +159,8 @@ in
     programs.vscode.package = pkgs.vscodium; 
     programs.vscode.extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
-      pkgs.vscode-extensions.bbenoist.Nix
+      bbenoist.nix
+      scala-lang.scala
     ];
 
     # gnome terminal
@@ -135,7 +172,7 @@ in
     programs.git.userEmail = "not.my.email.yet@gmail.com";
     programs.git.extraConfig.diff.tool = "kdiff3";
     programs.git.extraConfig.merge.tool = "kdiff3";
-    programs.git.extraConfig.difftool.prompt = "false";
+    # programs.git.extraConfig.difftool.prompt = "false";
   };
 
 }
